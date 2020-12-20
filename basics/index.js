e06e2bc8b0bd4f10493abd2ca14ff72931aaa9ce
codwerk <louis_widdy@live.fr>
2020-12-20 19:09:23 +0100

    part 1

--- basics/index.js
+++ basics/index.js
@@ -1,1 +1,23 @@
-console.log('hello, vues.js 3');
+const app = Vue.createApp({
+
+  //template: '<h2>Je suis un template</h2>',
+  data() {
+    return {
+      title: 'j\'apprend vue js3 ',
+      author: 'Codwerk',
+      age: 25,
+      showText: true
+    }
+  },
+  methods: {
+    change(title) {
+      //this.title = 'encore et encore du vue mais vue js 3'
+      this.title = title
+    },
+    showEl() {
+      this.showText = !this.showText;
+    }
+  }
+});
+
+app.mount('#app')
