396f45a161be6c9b5ae86d92cbc787e36a3a03f1
codwerk <louis_widdy@live.fr>
2021-01-10 17:13:45 +0100

    Outputting lists(v-for)

--- basics/index.js
+++ basics/index.js
@@ -4,7 +4,18 @@ const app = Vue.createApp({
   data() {
     return {
       x: 0,
-      y: 0
+      y: 0,
+      books: [
+        {
+          title: 'name of the wind', author: 'patrick'
+        },
+        {
+          title: 'the way of kings', author: 'ben'
+        },
+        {
+          title: 'the final empire', author: 'stan'
+        }
+      ]
     }
   },
   methods: {
