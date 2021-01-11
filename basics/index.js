50e64cef472d73bbf4ac578e638391bf617e17e1
codwerk <louis_widdy@live.fr>
2021-01-11 21:47:23 +0100

    Attribute binding

--- basics/index.js
+++ basics/index.js
@@ -7,15 +7,16 @@ const app = Vue.createApp({
       y: 0,
       books: [
         {
-          title: 'name of the wind', author: 'patrick'
+          title: 'name of the wind', author: 'patrick', img: 'https://avatars1.githubusercontent.com/u/36073831?s=460&u=f07ac15aa1000b4aee77aba9ece5c276559732dc&v=4'
         },
         {
-          title: 'the way of kings', author: 'ben'
+          title: 'the way of kings', author: 'ben', img: 'https://yt3.ggpht.com/ytc/AAUvwni-Wm0ZBDYJYFwySP2esxgc8s9mKiwmI3yJrmjlHQ=s88-c-k-c0xffffffff-no-rj-mo'
         },
         {
-          title: 'the final empire', author: 'stan'
+          title: 'the final empire', author: 'stan', img: 'https://vignette.wikia.nocookie.net/onepiece/images/8/89/Wiki-wordmark.png/revision/latest?cb=20180401015943&path-prefix=fr'
         }
-      ]
+      ],
+      url:'https://codwerk.github.io/'
     }
   },
   methods: {
