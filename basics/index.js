07be8dfd2a3bb9172cf050b33c05b5a0c2809dfe
codwerk <louis_widdy@live.fr>
2021-01-12 22:24:47 +0100

    Dynamic classes

--- basics/index.js
+++ basics/index.js
@@ -7,13 +7,13 @@ const app = Vue.createApp({
       y: 0,
       books: [
         {
-          title: 'name of the wind', author: 'patrick', img: 'https://avatars1.githubusercontent.com/u/36073831?s=460&u=f07ac15aa1000b4aee77aba9ece5c276559732dc&v=4'
+          title: 'name of the wind', author: 'patrick', img: 'https://avatars1.githubusercontent.com/u/36073831?s=460&u=f07ac15aa1000b4aee77aba9ece5c276559732dc&v=4', isFav: true
         },
         {
-          title: 'the way of kings', author: 'ben', img: 'https://yt3.ggpht.com/ytc/AAUvwni-Wm0ZBDYJYFwySP2esxgc8s9mKiwmI3yJrmjlHQ=s88-c-k-c0xffffffff-no-rj-mo'
+          title: 'the way of kings', author: 'ben', img: 'https://yt3.ggpht.com/ytc/AAUvwni-Wm0ZBDYJYFwySP2esxgc8s9mKiwmI3yJrmjlHQ=s88-c-k-c0xffffffff-no-rj-mo', isFav: false
         },
         {
-          title: 'the final empire', author: 'stan', img: 'https://vignette.wikia.nocookie.net/onepiece/images/8/89/Wiki-wordmark.png/revision/latest?cb=20180401015943&path-prefix=fr'
+          title: 'the final empire', author: 'stan', img: 'https://vignette.wikia.nocookie.net/onepiece/images/8/89/Wiki-wordmark.png/revision/latest?cb=20180401015943&path-prefix=fr', isFav: true
         }
       ],
       url:'https://codwerk.github.io/'
