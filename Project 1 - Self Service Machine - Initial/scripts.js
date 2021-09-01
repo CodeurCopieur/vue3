5db7a091d202eceb4e6856abe131fe55bd908155
codwerk <louisws@outlook.fr>
2021-09-01 19:12:44 +0200

    first commit

---
+++ Project 1 - Self Service Machine - Initial/scripts.js
@@ -0,0 +1,66 @@
+var products = [
+    {
+        "photo": "img/big-mac.png",
+        "name": "Big Mac",
+        "price": 5.99,
+        "active": false,
+        "quantity": 1
+    },
+    {
+        "photo": "img/mc-chicken.png",
+        "name": "Mc Chicken",
+        "price": 4.99,
+        "active": false,
+        "quantity": 1
+    },
+    {
+        "photo": "img/double-cb.png",
+        "name": "Double Cheese Burger",
+        "price": 2.99,
+        "active": false,
+        "quantity": 1
+    },
+    {
+        "photo": "img/fries.png",
+        "name": "Fries",
+        "price": 2.99,
+        "active": false,
+        "quantity": 1
+    },
+    {
+        "photo": "img/nuggets.png",
+        "name": "Mc Nuggets",
+        "price": 3.49,
+        "active": false,
+        "quantity": 1
+    },
+    {
+        "photo": "img/salad.png",
+        "name": "Salad",
+        "price": 2.79,
+        "active": false,
+        "quantity": 1
+    },
+    {
+        "photo": "img/cola.png",
+        "name": "Coke",
+        "price": 1.99,
+        "active": false,
+        "quantity": 1
+    },
+    {
+        "photo": "img/lipton.png",
+        "name": "Ice Tea",
+        "price": 1.99,
+        "active": false,
+        "quantity": 1
+    },
+    {
+        "photo": "img/water.png",
+        "name": "Water",
+        "price": 1.49,
+        "active": false,
+        "quantity": 1
+    }
+];
+ 
