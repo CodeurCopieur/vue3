bec7003cf03aefb18323f26eb40f6c1d0954442f
codwerk <louisws@outlook.fr>
2021-09-05 00:54:55 +0200

    first commit

---
+++ Project 2 - Working With Forms - Initial/scripts.js
@@ -0,0 +1,24 @@
+members = [
+    {
+        fname: 'John',
+        lname: 'Lennon',
+        instrument: 'Acoustic Guitar'
+    },
+    {
+        fname: 'George',
+        lname: 'Harrison',
+        instrument: 'Electric Guitar'
+    }
+]
+
+const handlingForms = {
+
+    data() {
+        return {
+            members
+        }
+    },
+
+};
+
+Vue.createApp(handlingForms).mount('#app');
