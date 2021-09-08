6c7306e368656c308f736cda8fe6d0a65f800cc4
codwerk <louisws@outlook.fr>
2021-09-08 12:07:37 +0200

    Chargement des données à partir du Localstorage

--- Project 3 - Todo List - Initial/scripts.js
+++ Project 3 - Todo List - Initial/scripts.js
@@ -11,11 +11,15 @@ const todosApp = {
       if(this.newTodo.text) {
         this.todos.push(this.newTodo);
         this.newTodo = { done:false }
+        localStorage.setItem('todos', JSON.stringify(this.todos));
       } else {
         alert('le texte est nécessaire !')
       }
        
     }
+  },
+  created() {
+    this.todos = localStorage.getItem('todos') ? JSON.parse(localStorage.getItem('todos')) : this.todos;
   }
 };
 
