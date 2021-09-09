5454fe53c731c00d82c46f2d18abd2322bfc9ff0
codwerk <louisws@outlook.fr>
2021-09-09 22:11:03 +0200

    installer axios

--- project-4_quiz-game-initial/src/main.js
+++ project-4_quiz-game-initial/src/main.js
@@ -1,4 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
-createApp(App).mount('#app')
+createApp(App).mount('#app').use(VueAxios, axios)
