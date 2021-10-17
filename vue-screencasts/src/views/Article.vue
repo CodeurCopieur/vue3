f95b945dff3fef129587110ab12f91836e53bacd
codwerk <louisws@outlook.fr>
2021-10-18 00:59:26 +0200

    page article

---
+++ vue-screencasts/src/views/Article.vue
@@ -0,0 +1,27 @@
+<template>
+  <main>
+    <div class="component__wrap-content-aside">
+      <div class="component__content">
+        <article class="component__wrap-details">
+          <img :src="video.thumbnail" alt="">
+          <h1>{{ video.name }}</h1>
+          <p>{{ this.$route.params.id }}</p>
+          <div v-html="video.description"></div>
+        </article>
+      </div>
+      
+      <aside class="component__content-moreInfos"></aside>
+    </div>
+    
+  </main>
+  
+</template>
+<script>
+export default {
+  computed: {
+    video() {
+      return this.$store.state.videos.find( video => video.id == this.$route.params.id)
+    }
+  }
+}
+</script>
