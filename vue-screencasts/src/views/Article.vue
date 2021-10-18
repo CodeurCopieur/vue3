f8c0383f52f58b8625078d8093cb5df6be800e6f
codwerk <louisws@outlook.fr>
2021-10-18 14:48:23 +0200

    inte html/css

--- vue-screencasts/src/views/Article.vue
+++ vue-screencasts/src/views/Article.vue
@@ -3,14 +3,22 @@
     <div class="component__wrap-content-aside">
       <div class="component__content">
         <article class="component__wrap-details">
-          <img :src="video.thumbnail" alt="">
+
+          <figure>
+            <picture class="component__wrap-image">
+              <img :src="video.thumbnail" alt="">
+            </picture>
+          </figure>
+          
           <h1>{{ video.name }}</h1>
           <p>{{ this.$route.params.id }}</p>
           <div v-html="video.description"></div>
         </article>
       </div>
       
-      <aside class="component__content-moreInfos"></aside>
+      <aside class="component__content-more">
+        <h1>aside</h1>
+      </aside>
     </div>
     
   </main>
@@ -25,3 +33,52 @@ export default {
   }
 }
 </script>
+
+<style lang="scss">
+
+  
+@mixin aspect-ratio($width, $height, $displayType: block, $important:false) {
+
+  display: $displayType;
+  position: relative;
+  height: 0;
+
+  @if $important {
+    padding-top: ($height / $width) * 100% !important;
+  }
+  @else {
+    padding-top: ($height / $width) * 100%;
+  }
+
+  img {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+}
+
+  .component__wrap-content-aside {
+    display: flex;
+    flex-direction: row;
+
+    max-width: 80%;
+    margin-left: auto;
+    margin-right: auto;
+  }
+
+  .component__content {
+    width: 60%;
+  }
+
+  .component__wrap-image{
+    @include aspect-ratio(16, 9);
+  }
+
+  .component__content-more {
+    width: 30%;
+    margin-left: 20px;
+  }
+</style>
