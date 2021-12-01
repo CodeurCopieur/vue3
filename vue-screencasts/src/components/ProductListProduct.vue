ed69113d53e89ee774155e8e6487c1e7efc2dd09
codwerk <louisws@outlook.fr>
2021-12-01 15:27:29 +0100

    Create ProductListProduct.vue

---
+++ vue-screencasts/src/components/ProductListProduct.vue
@@ -0,0 +1,69 @@
+<template>
+  <article class="component-products__card">
+      <router-link :to="{name: 'product-watch', params: { id:product.id}}">
+        <figure>
+          <picture class="component-products__wrap-image">
+            <img :src="product.thumbnail" alt="">
+          </picture>
+        </figure>
+        <h3>{{ product.name }}</h3>
+        <div v-html="product.description"></div>
+      </router-link>
+  </article>
+
+</template>
+
+<script>
+export default {
+  name: 'ProductListProduct',
+  props: ['product']
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+@mixin aspect-ratio($width, $height, $displayType: block, $important:false) {
+
+  display: $displayType;
+  position: relative;
+  height: 0;
+
+  @if $important {
+    padding-top: ($height / $width) * 100% !important;
+  }
+  @else {
+    padding-top: ($height / $width) * 100%;
+  }
+
+  img {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+}
+
+
+.component-products{
+  &__card {
+      flex-basis: calc((100% - 2 * 20px)/3);
+      font-size: .8125em;
+      margin-top: 20px;
+      position: relative;
+      overflow: hidden;
+
+      &:not(:nth-child(3n-2)) {
+        margin-left: 20px;
+      }
+    }
+
+    &__wrap-image {
+       @include aspect-ratio(16, 9);
+    }
+}
+    
+</style>
