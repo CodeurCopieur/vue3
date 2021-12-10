8793438cef750c17bfbc155f276673597d6379d1
codwerk <louisws@outlook.fr>
2021-12-10 23:43:43 +0100

    reactive

---
+++ api-composition/src/components/Majuscules-reactive.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="maj">
+    <h2>Composation 2</h2>
+    <input type="text" name="" id="" v-model="state.entree" @keyup="Maj">
+    <span>{{ state.sortie}}</span>
+  </div>
+</template>
+
+<script>
+
+import {reactive} from 'vue';
+
+export default {
+  name: 'Majuscules',
+  setup() {
+    let state = reactive({
+      entree: '',
+      sortie: ''
+    });
+
+    function Maj(){
+      state.sortie = state.entree.toUpperCase();
+    }
+
+    return {
+      state,
+      Maj
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
