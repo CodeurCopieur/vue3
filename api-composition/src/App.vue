5ff2f4e1ddc5e4246ac16a6e13315570456b1674
codwerk <louisws@outlook.fr>
2021-12-10 22:16:58 +0100

    L'API de composition

---
+++ api-composition/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <Majuscules/>
+</template>
+
+<script>
+import Majuscules from '@/components/Majuscules'
+
+export default {
+  name: 'App',
+  components: {
+    Majuscules
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ api-composition/src/components/Majuscules.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="maj">
+    <h2>Composation 2</h2>
+    <input type="text" name="" id="" v-model="entree" @keyup="Maj">
+    <span>{{ sortie}}</span>
+  </div>
+</template>
+
+<script>
+
+import {ref} from 'vue';
+
+export default {
+  name: 'Majuscules',
+  setup() {
+    let entree = ref('');
+    let sortie = ref('');
+
+    function Maj(){
+      sortie.value = entree.value.toUpperCase();
+    }
+
+    return {
+      entree,
+      sortie,
+      Maj
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
