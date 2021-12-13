799c4943ff87b016f4872b46f5dbb143de78f39a
codwerk <louisws@outlook.fr>
2021-12-13 17:08:44 +0100

    displaying error message

--- vue-3-tailwind/src/components/Vuelidate.vue
+++ vue-3-tailwind/src/components/Vuelidate.vue
@@ -2,7 +2,7 @@
   <div class="bg-gray-200 flex content-center justify-center items-center h-full">
     <div class="w-full max-w-sm">
       <form class="bg-white shadow-md rounded px-8 py-8 mb-4" @submit="submitForm">
-        <h1 class="text-2xl font-bold text-gray-800 mb-7">Create an account</h1>
+        <h1 class="text-2xl font-bold text-gray-800 mb-6">Create an account</h1>
         <div class="mb-7">
           <input
             class="
@@ -17,18 +17,22 @@
               leading-tight
               focus:outline-none focus:shadow-outline
             "
+            :class="v$.email.$error ? 'border-red-500' : 'border-green-500'"
             v-model='state.email'
             id="username"
             type="text"
             placeholder="Username"
           />
+          <p class="text-red-500 text-xs italic" v-if="v$.email.$error">
+            {{v$.email.$errors[0].$message}}
+          </p>
         </div>
         <div class="mb-6">
           <input
             class="
               shadow
               appearance-none
-              border border-red-500
+              border
               rounded
               w-full
               py-2
@@ -38,12 +42,15 @@
               leading-tight
               focus:outline-none focus:shadow-outline
             "
+            :class="v$.password.password.$error ? 'border-red-500' : 'border-green-500'"
             v-model='state.password.password'
             id="password"
             type="password"
             placeholder="Password"
           />
-          <p class="text-red-500 text-xs italic"></p>
+          <p class="text-red-500 text-xs italic" v-if="v$.password.password.$error">
+            {{v$.password.password.$errors[0].$message}}
+          </p>
         </div>
 
         <div class="mb-6">
@@ -61,12 +68,15 @@
               leading-tight
               focus:outline-none focus:shadow-outline
             "
+            :class="v$.password.confirm.$error ? 'border-red-500' : 'border-green-500'"
             v-model="state.password.confirm"
             id="confirm"
             type="password"
             placeholder="Confirm Password"
           />
-          <p class="text-red-500 text-xs italic"></p>
+          <p class="text-red-500 text-xs italic" v-if="v$.password.confirm.$error">
+            {{v$.password.confirm.$errors[0].$message}}
+          </p>
         </div>
         <div class="flex items-center justify-center">
           <button
@@ -130,6 +140,7 @@ export default {
   methods: {
     submitForm(e) {
       e.preventDefault();
+      console.log(this.v$);
       this.v$.$validate()
       if(!this.v$.$error) {
         alert('form success !');
