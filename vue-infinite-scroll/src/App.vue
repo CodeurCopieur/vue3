16e815fbd13621e113964230229352d9e9ca6358
codwerk <louisws@outlook.fr>
2021-12-25 19:35:03 +0100

    import ListComponent

--- vue-infinite-scroll/src/App.vue
+++ vue-infinite-scroll/src/App.vue
@@ -1,11 +1,11 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import ListComponent from './components/ListComponent.vue'
 </script>
 
 <template>
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <ListComponent/>
 </template>
 
 <style>
