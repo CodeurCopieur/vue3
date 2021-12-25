7bfec70a52c807c323158ad778ca87ca9cad619e
codwerk <louisws@outlook.fr>
2021-12-25 19:21:50 +0100

    Making ListComponent

---
+++ vue-infinite-scroll/src/components/ListComponent.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="scrolling-component" ref="scrollingComponent">
+    <PostComponent
+      v-for="(post, i) in posts"
+      :post="post"
+      :key="i"
+    ></PostComponent>
+  </div>
+</template>
+
+<script>
+
+  import getPosts from '../api/get-posts'
+  import PostComponent from './PostComponent.vue'
+  import {ref, onMounted, onUnmounted} from 'vue'
+
+  export default {
+    components: {
+      PostComponent
+    },
+    setup() {
+
+      const posts = ref(getPosts(10))
+
+      return {
+        posts
+      }
+
+    }
+  }
+</script>
+
+<style>
+
+</style>
