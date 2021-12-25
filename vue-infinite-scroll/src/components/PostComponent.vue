46d86e77c7379026952ea4ec0f7e724396e7b3cb
codwerk <louisws@outlook.fr>
2021-12-25 19:21:40 +0100

    Making PostComponent

---
+++ vue-infinite-scroll/src/components/PostComponent.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="post p-6 border-b-4 border-indigo-500 last:border-b-0">
+    <h2>{{post.author}}</h2>
+    <p>{{post.content}}</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      post: Object
+    }
+  }
+</script>
