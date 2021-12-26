cce7454036764366b433012bf1933c8b819b013e
codwerk <louisws@outlook.fr>
2021-12-26 01:01:28 +0100

    IntersectionObserver

--- vue-infinite-scroll/src/components/ListComponent.vue
+++ vue-infinite-scroll/src/components/ListComponent.vue
@@ -1,18 +1,19 @@
 <template>
-  <div class="scrolling-component max-w-sm mx-auto bg-white" ref="scrollingComponent">
+  <div class="scrolling-component max-w-sm mx-auto bg-white">
     <PostComponent
       v-for="(post, i) in posts"
       :post="post"
       :key="i"
     ></PostComponent>
+    <div  ref="scrollingComponent"> test</div>
   </div>
 </template>
 
 <script>
 
-  import getPosts from '../api/get-posts'
   import PostComponent from './PostComponent.vue'
   import {ref, onMounted, onUnmounted} from 'vue'
+  import getPosts from '../api/get-posts'
 
   export default {
     components: {
@@ -21,9 +22,34 @@
     setup() {
 
       const posts = ref(getPosts(10))
+      const scrollingComponent = ref(null)
+
+      const loadMorePosts = () => {
+        let newPosts = getPosts(10)
+        posts.value.push(...newPosts)
+      }
+
+    const scrollTriger = ()=> {
+      
+       const observer = new IntersectionObserver( (entries) => {
+         entries.forEach( entry => {
+           if(entry.intersectionRatio > 0 ) {
+             setTimeout(()=> {
+               loadMorePosts()
+             })
+           }
+         });
+       });
+       observer.observe(scrollingComponent.value);
+     }
+
+      onMounted(()=> {
+        scrollTriger()
+      })
 
       return {
-        posts
+        posts,
+        scrollingComponent
       }
 
     }
