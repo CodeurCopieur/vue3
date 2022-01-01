febe6a3765afbc74780fa478cbeaf6dcaf652d6b
codwerk <louisws@outlook.fr>
2022-01-01 12:25:20 +0100

    Component  Skeleton

---
+++ skeleton-loading-screen/src/components/ProfileCardSkeleton.vue
@@ -0,0 +1,11 @@
+<template>
+<div class="p-2 lg:w-1/3 md:w-1/2 w-full">
+              <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+                <img class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4 bg-gray-400">
+                <div class="flex-grow">
+                  <h2 class="title-font font-medium w-full bg-zinc-700 rounded-lg p-3 mb-2"></h2>
+                  <p class="bg-zinc-500 rounded-lg p-7"></p>
+                </div>
+              </div>
+          </div>        
+</template>
