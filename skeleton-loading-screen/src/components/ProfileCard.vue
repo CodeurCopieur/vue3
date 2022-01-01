c981fd3ffe1abc4ec4b71aace52eb035aff829ef
codwerk <louisws@outlook.fr>
2022-01-01 12:25:53 +0100

    Update ProfileCard.vue

--- skeleton-loading-screen/src/components/ProfileCard.vue
+++ skeleton-loading-screen/src/components/ProfileCard.vue
@@ -1,25 +1,14 @@
 <template>
-  <section class="text-gray-600 body-font">
-    <div class="container px-5 py-24 mx-auto">
-      <div class="flex flex-col text-center w-full mb-20">
-        <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">Our Team</h1>
-        <p class="lg:w-2/3 mx-auto leading-relaxed text-base">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify, subway tile poke farm-to-table. Franzen you probably haven't heard of them.</p>
+  <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
+      <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+        <img class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" 
+          :src="userData.pic">
+        <div class="flex-grow">
+          <h2 class="text-gray-900 title-font font-medium">{{ userData.name }}</h2>
+          <p class="text-gray-500">{{ userData.bio }}</p>
+        </div>
       </div>
-
-      <div class="flex flex-wrap -m-2">
-        <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
-            <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
-              <img alt="team" class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" 
-                :src="userData.pic">
-              <div class="flex-grow">
-                <h2 class="text-gray-900 title-font font-medium">{{ userData.name }}</h2>
-                <p class="text-gray-500">{{ userData.bio }}</p>
-              </div>
-            </div>
-          </div>        
-      </div>
-    </div>
-  </section>
+  </div>                
 </template>
 
 <script>
