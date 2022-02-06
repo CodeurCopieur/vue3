d99cb49c8569b58be18e859397c919c0d11083bf
Widdy LOUIS <louisws@outlook.fr>
2022-02-06 12:39:19 +0100

    first commit

---
+++ vue-and-axios_kanye-rest-api/README.md
@@ -0,0 +1,22 @@
+# Vue 3 and Axios (feat. Kanye REST API)
+
+Nous verrons : 
+
+- axios dans une application Vue 3
+- connecter nos applications Vue aux API
+- organiser votre projet Vue avec des appels d'API réutilisables
+- les avantages d'utiliser le dossier src/services pour organiser vos appels API !
+
+## Setup vite
+
+- npm init vite
+- project name : ...
+- select a framework : ...
+
+## Setup axios
+
+- npm install axios
+
+## start
+
+- npm run dev
---
+++ vue-and-axios_kanye-rest-api/src/App.vue
@@ -0,0 +1,30 @@
+<script setup>
+  import axios from 'axios'
+  import { ref } from 'vue'
+
+  const quote = ref('');
+
+  axios.get('https://api.kanye.rest/')
+    .then( res => quote.value = res.data.quote)
+
+
+
+</script>
+
+<template>
+  <div>
+    <i>"{{ quote}}"</i>
+    <p>- kanye west</p>
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
