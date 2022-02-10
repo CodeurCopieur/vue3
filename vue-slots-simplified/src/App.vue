2603eed63ed4be55351d1c6135b7d55482398132
Widdy LOUIS <louisws@outlook.fr>
2022-02-11 00:02:20 +0100

    named slots

--- vue-slots-simplified/src/App.vue
+++ vue-slots-simplified/src/App.vue
@@ -4,7 +4,14 @@
 
 <template>
   <CaptionedContent>
-    source : <a href="https://codesandbox.io/u/codwerk" target="_blank" class="px-3 py-2 rounded-md bg-sky-500 text-white cursor-pointer"><span class="sm:inline lg:hidden xl:inline"> Codwerk</span></a>
+    source : <a href="https://codesandbox.io/u/codwerk" target="_blank" class="px-3 py-2 rounded-md bg-sky-500 text-white cursor-pointer"><span class="sm:inline xl:inline"> codesandbox</span></a>
+
+    <template #content>
+      <img src="https://tailwindcss.com/_next/static/media/5.cb3efb05f0114abf9efaa0ddbeaed688.jpg" alt="" class="w-full h-96 object-cover rounded-lg sm:h-60 sm:col-span-2 lg:col-span-full" loading="lazy">
+    </template>
+    <template #caption>
+      source : <a href="https://codepen.io/codwerk" target="_blank" class="px-3 py-2 rounded-md bg-sky-500 text-white cursor-pointer"><span class="sm:inline xl:inline"> codepen</span></a>
+    </template>
   </CaptionedContent>
 </template>
 
--- vue-slots-simplified/src/components/CaptionedContent.vue
+++ vue-slots-simplified/src/components/CaptionedContent.vue
@@ -1,6 +1,5 @@
 <template>
   <main class="py-6 px-4 sm:p-6 md:py-10 md:px-8">
-    <div >
       <figure class="max-w-4xl mx-auto grid grid-cols-1 lg:max-w-5xl lg:gap-x-20 lg:grid-cols-2">
         <img src="https://tailwindcss.com/_next/static/media/beach-house.02381ba1b6293047997200a3099d03cb.jpg" alt="" class="w-full h-96 object-cover rounded-lg sm:h-60 sm:col-span-2 lg:col-span-full" loading="lazy">
         <div class="pt-6 md:p-8 text-center md:text-left space-y-4">
@@ -11,7 +10,20 @@
             </figcaption>
         </div>
       </figure>
-    </div>
+
+      <figure class="max-w-4xl mx-auto grid grid-cols-1 lg:max-w-5xl lg:gap-x-20 lg:grid-cols-2">
+        <slot name="content">
+          <img src="https://tailwindcss.com/_next/static/media/beach-house.02381ba1b6293047997200a3099d03cb.jpg" alt="" class="w-full h-96 object-cover rounded-lg sm:h-60 sm:col-span-2 lg:col-span-full" loading="lazy">
+        </slot>
+        <div class="pt-6 md:p-8 text-center md:text-left space-y-4">
+            <figcaption class="font-medium">
+              <div class="text-slate-700 dark:text-slate-500">
+                <slot name="caption">Caption</slot>
+              </div>
+            </figcaption>
+        </div>
+      </figure>
+
   </main>
 </template>
 
