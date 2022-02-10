8e15da92e35360322f0b36d5456bee0f9418da40
Widdy LOUIS <louisws@outlook.fr>
2022-02-11 00:15:27 +0100

    data with slot

--- vue-slots-simplified/src/App.vue
+++ vue-slots-simplified/src/App.vue
@@ -1,5 +1,9 @@
 <script setup>
+  import { ref } from 'vue'
   import CaptionedContent from './components/CaptionedContent.vue'
+
+  const source = ref('codepen')
+  const link = ref('https://codepen.io/codwerk')
 </script>
 
 <template>
@@ -10,7 +14,7 @@
       <img src="https://tailwindcss.com/_next/static/media/5.cb3efb05f0114abf9efaa0ddbeaed688.jpg" alt="" class="w-full h-96 object-cover rounded-lg sm:h-60 sm:col-span-2 lg:col-span-full" loading="lazy">
     </template>
     <template #caption>
-      source : <a href="https://codepen.io/codwerk" target="_blank" class="px-3 py-2 rounded-md bg-sky-500 text-white cursor-pointer"><span class="sm:inline xl:inline"> codepen</span></a>
+      source : <a :href="link" target="_blank" class="px-3 py-2 rounded-md bg-sky-500 text-white cursor-pointer"><span class="sm:inline xl:inline"> {{source}} :)</span></a>
     </template>
   </CaptionedContent>
 </template>
