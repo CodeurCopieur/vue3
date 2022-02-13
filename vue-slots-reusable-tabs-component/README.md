c0b4d71a808c5faa1c98600a9250fbea89d834e7
Widdy LOUIS <louisws@outlook.fr>
2022-02-13 13:53:51 +0100

    first commit

---
+++ vue-slots-reusable-tabs-component/README.md
@@ -0,0 +1,8 @@
+# Vue Slots : Reusable Tabs Component
+
+J'utilise des emplacements pour créer un composant d'onglet personnalisé qui permet de transformer tout type de contenu en un système d'onglets à une seule page.
+
+- Tabs.vue : contient le contenu individuel de chaque onglet
+- TabsWrapper.vue : tous les onglets seront situé à l'intérieur, gère le rendu conditionnel
+
+>> les 2 utiliseront des slots
---
+++ vue-slots-reusable-tabs-component/src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import Tab from './components/Tab.vue'
+import TabsWrapper from './components/TabsWrapper.vue'
+
+</script>
+
+<template>
+  <TabsWrapper>
+    <Tab title="Tab 1">Hello Tab 1</Tab>
+    <Tab title="Tab 2">Hello Tab 2</Tab>
+    <Tab title="Tab 3">Hello Tab 3</Tab>
+    <Tab title="Tab 4">Hello Tab 4</Tab>
+  </TabsWrapper>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
