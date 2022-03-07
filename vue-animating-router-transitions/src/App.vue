c5c182a7c2456c2024c470a1f6023fd0e8c89ff8
Widdy LOUIS <louisws@outlook.fr>
2022-03-08 00:17:31 +0100

    add class tailwind

--- vue-animating-router-transitions/src/App.vue
+++ vue-animating-router-transitions/src/App.vue
@@ -3,7 +3,16 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <router-view></router-view>
+   <!-- 768px -->
+ <div class="max-w-3xl mx-auto mt-4">
+   <nav class="text-left">
+     <router-link class="p-2 font-bold mr-4 hover:bg-sky-500 hover:text-zinc-50" to="/">Home</router-link>
+     <router-link class="p-2 font-bold mr-4 hover:bg-sky-500 hover:text-zinc-50" to="/guide">Guide</router-link>
+   </nav>
+
+   <router-view></router-view>
+ </div>
+  
 </template>
 
 <style>
@@ -13,5 +22,10 @@ import HelloWorld from './components/HelloWorld.vue'
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+}
+
+a.router-link-active {
+  background-color: rgb(14, 165, 233);
+  color: rgb(250, 250, 250);
 }
 </style>
