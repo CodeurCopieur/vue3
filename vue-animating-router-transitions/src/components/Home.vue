a460525a62e2fd380e391092cce94636bdab3c34
Widdy LOUIS <louisws@outlook.fr>
2022-03-07 23:20:05 +0100

    create components (Home, Guide)

---
+++ vue-animating-router-transitions/src/components/Home.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="page">
+    <h1>Home</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, necessitatibus?</p>
+    <h2>Lorem ipsum.</h2>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem labore inventore sint natus dolores doloremque rerum. Unde facere saepe, quo repellendus, alias, minima rem accusantium quas quaerat ut distinctio numquam.
+    </p>
+  </div>
+</template>
