67fc39bdb31b0a37094b2b230caec909fda6933a
Widdy LOUIS <louisws@outlook.fr>
2022-03-16 00:14:41 +0100

    Carousel Slider

---
+++ vue-and-swiper_carousel-slider/src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import Carousel from './components/Carousel.vue'
+</script>
+
+<template>
+  <Carousel />
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  width: 1024px;
+  color: #2c3e50;
+  margin: 60px auto;
+}
+</style>
