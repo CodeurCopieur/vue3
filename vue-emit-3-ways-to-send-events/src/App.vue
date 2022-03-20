eac838f4f401037eabe571d7e6b4ee70d9f4111b
Widdy LOUIS <louisws@outlook.fr>
2022-03-20 11:05:58 +0100

    import component (Inline Emit)

---
+++ vue-emit-3-ways-to-send-events/src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import vueEmit from './components/inline-emit/vueEmit.vue'
+</script>
+
+<template>
+
+  <div style="margin-bottom: 60px;">
+    <h2>Inline Emit</h2>
+    <vueEmit/>
+  </div>
+  
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  
+}
+</style>
