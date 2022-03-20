9029a72b0ec570990ee6c5f6ab99bc17f51fe227
Widdy LOUIS <louisws@outlook.fr>
2022-03-20 11:26:54 +0100

    optionApiEmit component

---
+++ vue-emit-3-ways-to-send-events/src/components/inline-emit/optionApiEmit.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <p>Custom Text Input</p>
+    <input 
+      type="text"
+      placeholder="Custom Text Input"
+      @change="sendChange">
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    sendChange (event) {
+      this.$emit("optionApi", event.target.value)
+    }
+  }
+}
+</script>
