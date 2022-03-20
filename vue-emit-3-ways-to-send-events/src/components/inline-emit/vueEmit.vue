addb3cf8280d56fca8278c5fdf494a491d7be9b0
Widdy LOUIS <louisws@outlook.fr>
2022-03-20 11:26:37 +0100

    import  a other component(option api emit)

--- vue-emit-3-ways-to-send-events/src/components/inline-emit/vueEmit.vue
+++ vue-emit-3-ways-to-send-events/src/components/inline-emit/vueEmit.vue
@@ -1,25 +1,41 @@
 <template>
-  <inline-emit @customChange="logChange"></inline-emit>
-  <p>{{ TextInput }}</p>
+
+  <div style="margin-bottom: 60px;">
+    <h2>Inline Emit</h2>
+    <inline-emit @customChange="InlineEmit"></inline-emit>
+    <p>{{ first }}</p>
+  </div>
+
+  <div style="margin-bottom: 60px;">
+    <h2>Option Api</h2>
+    <option-api-emit @optionApi="optionApi"></option-api-emit>
+    <p>{{ second }}</p>
+  </div>
 </template>
 
 <script>
 
 import inlineEmit from './inlineEmit.vue'
+import optionApiEmit from './optionApiEmit.vue'
+
 
 export default {
   components: {
-    inlineEmit
+    inlineEmit, optionApiEmit
   },
   data() {
     return {
-      TextInput: ''
+      first: '',
+      second: ''
     }
   },
   methods: {
-    logChange (event) {
+    InlineEmit (event) {
       console.log(event);
-      this.TextInput =event
+      this.first = event;
+    },
+    optionApi (event) {
+      this.second = event;
     }
   }
 }
