89147c7443d3d1f1f64071087a583686e051ee42
Widdy LOUIS <louisws@outlook.fr>
2022-03-20 11:39:22 +0100

    component composition api emit

---
+++ vue-emit-3-ways-to-send-events/src/components/inline-emit/compositionApiEmit.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+    <p>Custom Text Input</p>
+    <input 
+      type="text"
+      placeholder="Custom Text Input"
+      @change="sendChange">
+  </div>
+</template>
+
+<script>
+export default {
+  setup(props, {emit}) {
+
+    const sendChange = (event) =>  {
+      emit("compositionApi", event.target.value)
+    }
+    
+    return {
+      sendChange
+    }
+  }
+
+ 
+}
+</script>
