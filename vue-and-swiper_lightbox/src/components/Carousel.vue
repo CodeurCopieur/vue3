bbdbfa51809c7827915140a3381c445319c066d0
Widdy LOUIS <louisws@outlook.fr>
2022-03-23 23:31:53 +0100

    Update Carousel.vue

--- vue-and-swiper_lightbox/src/components/Carousel.vue
+++ vue-and-swiper_lightbox/src/components/Carousel.vue
@@ -1,5 +1,5 @@
 <script setup>
-  import { Navigation,  Pagination, A11y, Virtual } from 'swiper'
+  import { Navigation,  Pagination, A11y, Lazy } from 'swiper'
   import { Swiper, SwiperSlide} from 'swiper/vue'
 
   import 'swiper/css'
@@ -10,13 +10,15 @@
   <Swiper 
     :slides-per-view="1" 
     :space-between="50" 
-    :modules="[Navigation, Pagination, A11y, Virtual]" 
+    :modules="[Navigation, Pagination, A11y, Lazy]" 
     navigation
     :pagination="{ clickable: true, dynamicBullets: true }"
     grab-cursor
-    virtual>
+    :preload-images="false"
+    lazy>
     <SwiperSlide v-for="n in 10" :key="n" :virtualIndex="n">
-      <img :src="'https://picsum.photos/1024/600?random='+n" alt="">
+      <img :src="'https://picsum.photos/1024/600?random='+n" class="swiper-lazy" alt="">
+      <div class="swiper-lazy-preloader"></div>
     </SwiperSlide>
   </Swiper>
 </template>
