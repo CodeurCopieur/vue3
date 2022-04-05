782069f2ae2850087e3271146e839eb3919d825e
Widdy LOUIS <louisws@outlook.fr>
2022-04-06 00:05:21 +0200

    inte html

--- vue-disneyplus-clone/src/views/Home.vue
+++ vue-disneyplus-clone/src/views/Home.vue
@@ -1,5 +1,37 @@
 <template>
-  <div class="home">
-    lol
+  <div class="home my-8">
+    <div class="mb-12">
+      <img src="../assets/slide1.jpg" class="rounded" alt="">
+    </div>
+    <div class="categories flex items-center mb-12 -mx-4">
+      <div class="w-1/5 px-4">
+        <a href="#" class="bg-gray-800 h-32 flex justify-center items-center rounded-lg border border-gray-700 p-4 hover:bg-gray-900 shadow-custom">
+          <img src="../assets/disney_logo.svg" alt="disney">
+        </a>
+      </div>
+      <div class="w-1/5 px-4">
+        <a href="#" class="bg-gray-800 h-32 flex justify-center items-center rounded-lg border border-gray-700 p-4 hover:bg-gray-900 shadow-custom">
+          <img src="../assets/pixar_logo.svg" alt="pixar">
+        </a>
+      </div>
+      <div class="w-1/5 px-4">
+        <a href="#" class="bg-gray-800 h-32 flex justify-center items-center rounded-lg border border-gray-700 p-4 hover:bg-gray-900 shadow-custom">
+          <img src="../assets/marvel_logo.svg" alt="marvel">
+        </a>
+      </div>
+      <div class="w-1/5 px-4">
+        <a href="#" class="bg-gray-800 h-32 flex justify-center items-center rounded-lg border border-gray-700 p-4 hover:bg-gray-900 shadow-custom">
+          <img src="../assets/star_wars_logo.svg" alt="star wars">
+        </a>
+      </div>
+      <div class="w-1/5 px-4">
+        <a href="#" class="bg-gray-800 h-32 flex justify-center items-center rounded-lg border border-gray-700 p-4 hover:bg-gray-900 shadow-custom">
+          <img src="../assets/national_geographic_logo.svg" alt="national geographic">
+        </a>
+      </div>
+    </div>
+    <p class="mb-32">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque voluptatum libero optio cum, vero, nisi maiores quae est itaque quaerat tempora asperiores recusandae id cupiditate facere iure dolore minima dolorem.
+    </p>
   </div>
 </template>
--- vue-disneyplus-clone/tailwind.config.js
+++ vue-disneyplus-clone/tailwind.config.js
@@ -1,7 +1,15 @@
 module.exports = {
   content: ['./index.html', './src/**/*{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    container: {
+      //padding: '1rem'
+    },
+    extend: {
+      boxShadow: {
+        custom: '0 20px 25px -5px rgb(0, 0, 0, .8), 0 10px 10px -5px rgb(0, 0, 0, 0.4)'
+      }
+        
+    }
   },
   plugins: [],
 }
