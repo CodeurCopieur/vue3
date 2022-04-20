899bb63dcf3b7ae5dc29944888d8b6d91ed277a3
Widdy LOUIS <louisws@outlook.fr>
2022-04-21 00:43:41 +0200

    Create App.vue

---
+++ vue-build-movie-app/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <header>
+    <router-link to="/">
+      <h1><span>Vue</span>Movies</h1>
+    </router-link>
+  </header>
+  <main>
+    <router-view />
+  </main>
+</template>
+
+<style lang="scss">
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Fira Sans', sans-serif;
+  }
+
+  body {
+    background-color: #193364;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  header {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 0.625rem 1rem;
+    background-color: #4186b4;
+    box-shadow: 0px 0px 0.375rem rgba(0, 0, 0, .1);
+
+    h1 {
+      color: #fff;
+      font-size: 1.75rem;
+
+      span {
+        color: #193364;
+      }
+    }
+  }
+</style>
