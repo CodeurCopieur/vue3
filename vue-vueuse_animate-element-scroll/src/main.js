2df982f2233b17b8810b5608900cf09d283745a8
Widdy LOUIS <louisws@outlook.fr>
2022-05-17 21:53:36 +0200

    add @vueuse/motion

--- vue-vueuse_animate-element-scroll/src/main.js
+++ vue-vueuse_animate-element-scroll/src/main.js
@@ -1,4 +1,6 @@
 import { createApp } from 'vue'
+import { MotionPlugin } from '@vueuse/motion'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+
+createApp(App).use(MotionPlugin).mount('#app')
