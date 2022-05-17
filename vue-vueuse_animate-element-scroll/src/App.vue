c9a186bf2258ae0cd9e731860df48b31f24e1b8e
Widdy LOUIS <louisws@outlook.fr>
2022-05-17 21:53:51 +0200

    end commit

--- vue-vueuse_animate-element-scroll/src/App.vue
+++ vue-vueuse_animate-element-scroll/src/App.vue
@@ -1,12 +1,25 @@
 <script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+  import { useMotion } from '@vueuse/motion'
+  import { ref } from 'vue'
+  const targetElt = ref()
+
+  useMotion(targetElt, {
+    initial: { opacity: 0, y: 100 },
+    visibleOnce: { opacity: 1, y: 0, delay: 200 }
+  });
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <div class="target" v-motion-slide-visible-once-right>1</div>
+  <div class="target" v-motion-slide-visible-once-right>2</div>
+  <div class="target" 
+    v-motion
+    :initial="{ opacity: 0, y: 100 }"
+    :visibleOnce="{ opacity: 1, y: 0, scale: 1.2 }"
+    :hovered="{ scale: 1.5 }"
+    :delay="200"
+    >3</div>
+  <div class="target" ref="targetElt">4</div>
 </template>
 
 <style>
@@ -17,5 +30,19 @@ import HelloWorld from './components/HelloWorld.vue'
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.target {
+  width: 100px;
+  height: 100px;
+  margin: 300px auto 500px auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: #fff;
+  border-radius: 10px;
+  background-color: #43b883;
+  font-weight: bold;
+  box-shadow: 0 2px 6px rgba(0, 0, 0, 0.15);
 }
 </style>
