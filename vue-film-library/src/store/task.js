da3f06c9f7a77a915ca8e0df86cd20372fb02cd2
Widdy LOUIS <louisws@outlook.fr>
2022-07-22 22:52:09 +0200

    add 2 functions in objet getters

--- vue-film-library/src/store/task.js
+++ vue-film-library/src/store/task.js
@@ -1,13 +1,19 @@
 export default {
   state:{
     tasks : [
-      { id: 1, title: 'title 1', description: 'description 1', whatWatch: 'Film', completed: false, editing: false },
-      { id: 2, title: 'title 2', description: 'description 2', whatWatch: 'Serial', completed: false, editing: false }
+      { id: 1, title: 'title 1', description: 'description 1', whatWatch: 'Film', completed: true, editing: false, tagsUsed: [{title: 'Horreur'}, {title: 'Action'}], time: '1 Heures 30 Minutes' },
+      { id: 2, title: 'title 2', description: 'description 2', whatWatch: 'Serial', completed: false, editing: false, tagsUsed: [{title: 'Horreur'}, {title: 'Action'}], time: '7 Heures 20 Minutes' }
     ]
   },
   getters: {
     getTasks(state) {
       return state.tasks
+    },
+    getCompletedTasks(state) {
+      return state.tasks.filter( task => task.completed)
+    },
+    getNotCompletedTasks(state) {
+      return state.tasks.filter( task => !task.completed)
     }
   },
   mutations: {
