bc2f3f1763ca515c221ea33be40233fe37e26253
Widdy LOUIS <louisws@outlook.fr>
2022-08-05 01:55:22 +0200

    end submit form

--- vue-film-library/src/views/Auth/Register.vue
+++ vue-film-library/src/views/Auth/Register.vue
@@ -8,7 +8,8 @@
     password: {
       password: '',
       confirm: ''
-    }
+    },
+    submitStatus: ''
   });
 
   const rules = computed(()=> {
@@ -22,8 +23,8 @@
           minLength: helpers.withMessage(({ $pending, $invalid, $params, $model }) =>  `Ce champ a une valeur de '${$model}' mais doit avoir une longueur minimale de ${$params.min} donc c'est ${$invalid ? 'invalide' : 'valide'}`, minLength(6))
         },
         confirm: { 
-          // required: helpers.withMessage("Le champ est vide", required),
-          sameAs: helpers.withMessage("Les mots de passe ne correspondent pas", sameAs(state.password.password))
+          required: helpers.withMessage("Le champ est vide", required),
+          sameAs: helpers.withMessage("Les mots de passe doivent être identiques.", sameAs(state.password.password))
         }
       }
     }
@@ -31,19 +32,29 @@
 
   const v$ = useVuelidate(rules, state)
 
-  const submitForm = async ()=> {
+  const onSubmit = async ()=> {
     const res = await v$.value.$validate();
     if(res) {
-      alert('oui', res);
+      state.submitStatus = "PENDING";
+
+      const user = {
+        email: state.email,
+        mdp: state.password.password
+      }
+
+      console.log(user);
+      setTimeout(() => {
+        state.submitStatus = "OK";
+      }, 500)
     } else {
-      alert('non');
+      state.submitStatus = "ERROR";
     }
   };
 
 </script>
 <template>
   <div class="auth__form">
-    <form action="" @submit.prevent="submitForm">
+    <form action="" @submit.prevent="onSubmit">
       <div class="sm:mx-auto md:mx-0 px-4 md:px-0">
         <div>
           <input type="email" 
@@ -63,15 +74,20 @@
         <div>
           <input type="password" 
             v-model.trim="state.password.confirm"
-            @blur="v$.password.confirm.$touch()"
+            @change="v$.password.confirm.$touch()"
             :class="{ 'border-red-500' : v$.password.confirm.$error, 'border-emerald-500': !v$.password.confirm.$invalid}" id="confirm" class="border-b border-gray-300 text-gray-900 text-sm block w-full p-2.5 mb-4 focus:border-emerald-400 focus:outline-none" placeholder="confirm ton mdp">
             <p class="text-red-500 text-xs italic" v-for="error in v$.password.confirm.$errors" :key="error.$uid"> {{error.$message}}</p>
         </div>
       </div>
 
       <button
+          :disabled="state.submitStatus === 'PENDING'"
           class="bg-emerald-500 hover:bg-emerald-700 text-white font-bold py-2 px-4 mt-4 focus:outline-none focus:shadow-outline"
           type="submit">Sign In</button>
+      
+      <p class="text-emerald-500 mt-4" v-if="state.submitStatus === 'OK'">Merci pour votre soumission!</p>
+      <p class="text-red-500 mt-4" v-if="state.submitStatus === 'ERROR'">Veuillez remplir le formulaire correctement.</p>
+      <p class="text-yellow-500 mt-4" v-if="state.submitStatus === 'PENDING'">Envoi en cours...</p>
     </form>
   </div>
 </template>
