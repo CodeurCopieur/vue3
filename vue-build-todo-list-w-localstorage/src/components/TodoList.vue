31d71854d5e0e80c034ddbb6b995391494f42c76
Widdy LOUIS <louisws@outlook.fr>
2022-12-25 19:20:39 +0100

    inte html

--- vue-build-todo-list-w-localstorage/src/components/TodoList.vue
+++ vue-build-todo-list-w-localstorage/src/components/TodoList.vue
@@ -5,7 +5,7 @@ const todos = ref([]) // tableau de tâches
 const name = ref('') // prénom du  user
 
 const input_content = ref('') // titre de la tâche
-const input_category = ref(null) // type de catégorie de la tâches (bussiness | personnel)
+const input_category = ref(null) // type de catégorie de la tâches (business | personal)
 
 const todo_asc = computed(() => todos.value.sort( (a,b ) => b.createdAt - a.createdAt))
 
@@ -22,9 +22,39 @@ const addTodo = () => {
   <main class="app">
     <section class="greeting">
       <h2 class="title">
-        Quoi de neuf, <input type="text" placeholder="Nom ici ?" v-model="name">
+        Quoi de neuf, <input type="text" placeholder="Nom ici ?" 
+          v-model="name">
       </h2>
     </section>
 
+    <section class="create-todo">
+      <h3>Créer une liste de choses à faire</h3>
+
+      <form @submit.prevent="addTodo">
+        <h4>Qu'y a-t-il sur votre liste de tâches</h4>
+
+        <input type="text" placeholder="Titre de la tâche" 
+          v-model="input_content">
+
+        <h4>choisissez une catégorie</h4>
+        <div class="options">
+          <label>
+            <input type="radio" name="category" id="category1" value="business"
+              v-model="input_category">
+            <span class="bubble business"></span>
+            <div>Business</div>
+          </label>
+          <label>
+            <input type="radio" name="category" id="category2" value="personal"
+              v-model="input_category">
+            <span class="bubble personal"></span>
+            <div>Business</div>
+          </label>
+        </div>
+
+        <input type="submit" value="ajouter une tâche">
+
+      </form>
+    </section>
   </main>
 </template>
