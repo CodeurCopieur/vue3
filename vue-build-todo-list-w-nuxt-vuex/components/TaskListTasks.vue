3a00f32f3a7090087dcc3387e3fd895b332b151e
Widdy LOUIS <louisws@outlook.fr>
2023-01-03 22:49:16 +0100

    Create TaskListTasks.vue

---
+++ vue-build-todo-list-w-nuxt-vuex/components/TaskListTasks.vue
@@ -0,0 +1,18 @@
+<template>
+  <div :class="`task ${ task.done ? 'is-complete' : ''}`">
+    <div class="content">
+      {{ task.content }}
+    </div>
+    <div class="buttons">
+      <button>{{ task.done ? 'Undo' : 'Done' }}</button>
+      <button class="delete">Delete</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TaskListProducts',
+  props: ['task']
+}
+</script>
